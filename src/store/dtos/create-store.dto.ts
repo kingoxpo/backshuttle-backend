df9baab6bbdbce89732b1b6402f3f576b54aee80
aiden <kingoxpo@gamil.com>
2021-10-15 10:45:47 +0900

    add class-validator

--- src/store/dtos/create-store.dto.ts
+++ src/store/dtos/create-store.dto.ts
@@ -1,15 +1,24 @@
 import { ArgsType, Field } from "@nestjs/graphql";
+import { IsBoolean, IsString, Length } from "class-validator";
 
 
 
 @ArgsType()
 export class createStoreDto {
     @Field(type => String)
+    @IsString()
+    @Length(5, 10)
     name:string;
+
     @Field(type => Boolean)
+    @IsBoolean()
     isCosmetic:boolean;
+
     @Field(type => String)
+    @IsString()
     address:string;
+
     @Field(type => String)
+    @IsString()
     ownerName:string;
 }
