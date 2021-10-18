bbbc4004d537a74e58c305a6dcd8d9a8edf1529a
aiden <kingoxpo@gamil.com>
2021-10-18 10:26:14 +0900

    add typeORM

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,23 @@
 import { Module } from '@nestjs/common';
 import { GraphQLModule } from '@nestjs/graphql';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { join } from 'path';
 import { StoreModule } from './store/store.module';
 
 @Module({
-  imports: [GraphQLModule.forRoot({
+  imports: [
+    StoreModule,
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'kingoxpo',
+      password: 'tnsqhr8516',
+      database: 'backshuttle',
+      synchronize: true,
+      logging: true
+    }),
+    GraphQLModule.forRoot({
     autoSchemaFile: true,
   }),
   StoreModule
