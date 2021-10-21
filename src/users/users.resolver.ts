dfc2595d915c5d344a80fc35e0e55f7192ddffa9
aiden <kingoxpo@gamil.com>
2021-10-21 14:08:08 +0900

    finished create account mutation

--- src/users/users.resolver.ts
+++ src/users/users.resolver.ts
@@ -7,12 +7,31 @@ import { UsersService } from "./users.service";
 
 @Resolver(of => User)
 export class UsersResolver{
-    constructor(private readonly userService: UsersService) {}
-    @Query(returns => Boolean)
-    user() {
-        return true;
+  constructor(private readonly userService: UsersService) {}
+  @Query(returns => Boolean)
+  user() {
+    return true;
+  }
+  @Mutation(returns => CreateAccountOutput)
+  async createAccount(
+    @Args('input') createAccountInput: CreateAccountInput,
+    ): Promise<CreateAccountOutput> {
+      try {
+        const error = await this.userService.createAccount(createAccountInput);
+        if (error){
+          return {
+            ok: false,
+            error,
+          };
+        }
+        return {
+          ok:true,
+        };
+      } catch(error) {
+        return {
+          error,
+          ok: false,
+        };
+      }
     }
-    @Mutation(returns => CreateAccountOutput)
-    createAccount(
-        @Args('input') createAccountInput: CreateAccountInput){}
 }
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,14 +1,35 @@
 import { Injectable } from "@nestjs/common";
 import { InjectRepository } from "@nestjs/typeorm";
 import { Repository } from "typeorm";
+import { CreateAccountInput } from "./dtos/create-account.dto";
 import { User } from "./entities/user.entity";
 
 
 
 @Injectable()
 export class UsersService {
-    constructor(
-        @InjectRepository(User)
-        private readonly users: Repository<User>,
-        ) {}
+  constructor(
+    @InjectRepository(User)
+    private readonly users: Repository<User>,
+    ) {}
+    async createAccount({
+      email,
+      password,
+      role,
+    }: CreateAccountInput): Promise<string | undefined> {
+      try {
+        const exists = await this.users.findOne({ email });
+        if (exists){
+          // make error
+          return '이미 가입된 이메일 주소입니다. 다른 이메일을 입력하여 주세요.';
+        }
+        await this.users.save(this.users.create({email, password, role}));
+      } catch(e) {
+        //make error
+        return '계정을 생성할 수 없습니다';
+      }
+      // check new user
+      // create user & hash the password
+
+    }
 }
