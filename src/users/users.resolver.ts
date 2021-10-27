1a6afd5aa0dfd794714c692ef08ecb3b8afed69e
aiden <kingoxpo@gamil.com>
2021-10-27 15:51:02 +0900

    finished updateProfile part

--- src/users/users.resolver.ts
+++ src/users/users.resolver.ts
@@ -3,6 +3,7 @@ import { Args, Context, Mutation, Query, Resolver } from "@nestjs/graphql";
 import { AuthUser } from "src/auth/auth-user.decorator";
 import { AuthGuard } from "src/auth/auth.guard";
 import { CreateAccountInput, CreateAccountOutput } from "./dtos/create-account.dto";
+import { EditProfileInput, EditProfileOutput } from "./dtos/edit-profile.dto";
 import { LoginInput, LoginOutput } from "./dtos/login.dto";
 import { UserProfileInput, UserProfileOutput } from "./dtos/user-profile.dto";
 import { User } from "./entities/user.entity";
@@ -12,14 +13,14 @@ import { UserService } from "./users.service";
 
 @Resolver(of => User)
 export class UserResolver{
-  constructor(private readonly userService: UserService) {}
+  constructor(private readonly usersService: UserService) {}
 
   @Mutation(returns => CreateAccountOutput)
   async createAccount(
     @Args('input') createAccountInput: CreateAccountInput,
     ): Promise<CreateAccountOutput> {
       try {
-        return this.userService.createAccount(createAccountInput);
+        return this.usersService.createAccount(createAccountInput);
       } catch(error) {
         return {
           error,
@@ -32,7 +33,7 @@ export class UserResolver{
     @Mutation(returns => LoginOutput)
     async login(@Args('input') loginInput: LoginInput): Promise<LoginOutput> {
       try {
-        return await this.userService.login(loginInput)
+        return await this.usersService.login(loginInput)
       } catch(error) {
         return {
           ok:false,
@@ -52,7 +53,7 @@ export class UserResolver{
     async userProfile(@Args() userProfileInput:UserProfileInput,
     ): Promise<UserProfileOutput> {
       try {
-        const user = await this.userService.findById(userProfileInput.userId);
+        const user = await this.usersService.findById(userProfileInput.userId);
         if(!user){
           throw Error();
         }
@@ -66,5 +67,25 @@ export class UserResolver{
           ok: false,
         };
       }
+    }
+    
+    @UseGuards(AuthGuard)
+    @Mutation(returns => EditProfileOutput)
+    async editProfile(
+      @AuthUser() authUser: User,
+      @Args('input') editProfileInput: EditProfileInput,
+    ): Promise<EditProfileOutput> {
+      try {
+        await this.usersService.editProfile(authUser.id, editProfileInput);
+        return {
+          ok:true,
+        }
+      } catch(error){
+        return {
+          ok:false,
+          error,
+        }
+      }
+
     }
 }
