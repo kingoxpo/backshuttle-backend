9eb294f0151583ae02162f0bfa719c2944032df9
aiden <kingoxpo@gamil.com>
2021-11-09 17:57:08 +0900

    add graphql-e2e user module test:createAccount

--- test/user.e2e-spec.ts
+++ test/user.e2e-spec.ts
@@ -4,6 +4,12 @@ import * as request from 'supertest';
 import { AppModule } from '../src/app.module';
 import { getConnection } from 'typeorm';
 
+jest.mock('got', () => {
+  return {
+    post: jest.fn(),
+  }
+})
+
 const GRAPHQL_ENDPOINT = '/graphql'
 
 describe('UserModule (e2e)', () => {
@@ -24,7 +30,7 @@ describe('UserModule (e2e)', () => {
 
   describe('createAccount', () => {
     const EMAIL = 'back@shuttle.com'
-    it('계정생성', () => {
+    it('계정생성 처리', () => {
       return request(app.getHttpServer()).post(GRAPHQL_ENDPOINT).send({
         query: `
         mutation {
@@ -43,8 +49,30 @@ describe('UserModule (e2e)', () => {
       .expect(res => {
         expect(res.body.data.createAccount.ok).toBe(true)
         expect(res.body.data.createAccount.error).toBe(null)
+      });
+    });
+
+    it('사용중인 아이디가 이미 있으면 실패처리', () => {
+      return request(app.getHttpServer()).post(GRAPHQL_ENDPOINT).send({
+        query: `
+        mutation {
+          createAccount(input:{
+            email:"${EMAIL}"
+            password: "12345"
+            role: Client
+          }) {
+            ok
+            error
+          }
+        }
+        `,
       })
-    })
+      .expect(200)
+      .expect(res => {
+        expect(res.body.data.createAccount.ok).toBe(false)
+        expect(res.body.data.createAccount.error).toEqual(expect.any(String))
+      })
+    });
   });
 
   it.todo('userProfile');
