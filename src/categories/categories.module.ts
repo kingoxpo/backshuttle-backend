bbee1871db3a138c9ebf58af3c0a3c014daf5fcd
aiden <kingoxpo@gamil.com>
2021-11-25 10:31:29 +0900

    modify categories module

--- src/categories/categories.module.ts
+++ src/categories/categories.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Store } from 'src/store/entities/store.entity';
 import { CategoryRepository } from 'src/store/repositories/category.repository';
+import { StoreService } from 'src/store/store.service';
 import { CategoryResolver } from './categories.resolver';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Store, CategoryRepository])],
-    providers: [CategoryResolver],
+    providers: [StoreService,CategoryResolver],
 })
 export class CategoriesModule {}
